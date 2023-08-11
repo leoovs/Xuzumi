ce63dcc7ed476387bc09266610428e8e4041423e
Leonid Ovsyannikov <lymuc.studio@gmail.com>
2023-08-11 22:54:43 +0300

    Add includes to the main header.

--- Engine/Inc/Xuzumi/Xuzumi.hpp
+++ Engine/Inc/Xuzumi/Xuzumi.hpp
@@ -1,7 +1,34 @@
 #ifndef XUZUMI_XUZUMI_HPP_
 #define XUZUMI_XUZUMI_HPP_
 
+#include "Xuzumi/Precompiled.hpp"
+
 #include "Xuzumi/Core/Compiler.hpp"
-#include "Xuzumi/Core/CommonTypes.hpp"
+#include "Xuzumi/Core/Delegate.hpp"
+#include "Xuzumi/Core/FuncName.hpp"
+#include "Xuzumi/Core/StringUtils.hpp"
+
+#include "Xuzumi/Debug/Logging.hpp"
+#include "Xuzumi/Debug/Assertion.hpp"
+#include "Xuzumi/Debug/DebugSession.hpp"
+#include "Xuzumi/Debug/FileLogWriter.hpp"
+#include "Xuzumi/Debug/ConsoleLogWriter.hpp"
+#include "Xuzumi/Debug/TimedLogFormatter.hpp"
+
+#include "Xuzumi/IDFramework/IDBase.hpp"
+#include "Xuzumi/IDFramework/HandleGenerator.hpp"
+
+#include "Xuzumi/Memory/UniquePtr.hpp"
+#include "Xuzumi/Memory/ObserverPtr.hpp"
+#include "Xuzumi/Memory/SharedFactory.hpp"
+
+#include "Xuzumi/Messaging/Event.hpp"
+#include "Xuzumi/Messaging/EventBus.hpp"
+#include "Xuzumi/Messaging/EventPublisher.hpp"
+#include "Xuzumi/Messaging/EventSubscriber.hpp"
+
+#include "Xuzumi/TypeMeta/TypeID.hpp"
+#include "Xuzumi/TypeMeta/TypeName.hpp"
+#include "Xuzumi/TypeMeta/TypeInfo.hpp"
 
 #endif // XUZUMI_XUZUMI_HPP_
